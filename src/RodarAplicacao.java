96e31f79c689fbeedee998da9315e10456f67b48
Abdiel Athayde <abdieldeathayde67@gmail.com>
2023-02-10 17:04:34 -0300

    Atualizando projeto upcast e downcast

---
+++ src/RodarAplicacao.java
@@ -0,0 +1,12 @@
+public class RodarAplicacao {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+
+        Funcionario gerente = new Gerente();
+        Funcionario vendedor = new Vendedor();
+        Funcionario faxineiro = new Faxineiro();
+
+        //Gerente gerente_ = new Funcionario();
+        Vendedor vendedor_ = (Vendedor) new Funcionario();
+    }
+}

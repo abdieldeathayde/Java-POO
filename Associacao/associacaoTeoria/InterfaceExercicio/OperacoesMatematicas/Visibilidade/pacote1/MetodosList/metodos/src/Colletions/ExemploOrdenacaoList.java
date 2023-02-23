9d89832a56279b1a4a3093e7f3e98826c2d03744
Abdiel Athayde <abdieldeathayde67@gmail.com>
2023-02-22 22:13:56 -0300

    adicionando exemplos bootcamp ListaEncadeada, ExemploList e ExemploOrdenacaoList

---
+++ Associacao/associacaoTeoria/InterfaceExercicio/OperacoesMatematicas/Visibilidade/pacote1/MetodosList/metodos/src/Colletions/ExemploOrdenacaoList.java
@@ -0,0 +1,88 @@
+package Colletions;
+
+/*Dadas as seguintes informações sobre meus gatos, crie uma lista
+e ordene esta lista exibindo:
+(nome - idade - cor);
+Gato 1 = nome: Jon, idade: 18, cor: preto
+Gato 2 = nome: Simba, idade: 6, cor: tigrado
+Gato 3 = nome: Jon, idade: 12, cor: amarelo
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import Colletions.Gato.ComparatorIdade;
+
+public class ExemploOrdenacaoList {
+    public static void main(String[] args) {
+        List<Gato> meusGatos = new ArrayList<>() {
+            {
+                add(new Gato("Jon", 18, "preto"));
+                add(new Gato("Simba", 6, "tigrado"));
+                add(new Gato("Jon", 12, "amarelo"));
+            }
+        };
+
+        System.out.println("--\tOrdem de Inseerção\t---");
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem aleatória\t---");
+        Collections.shuffle(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Natural (Nome)\t---");
+        Collections.sort(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("--\tOrdem Idade\t---");
+        // Collections.sort(meusGatos, new ComparatorIdade());
+        meusGatos.sort(new ComparatorIdade());
+        System.out.println(meusGatos);
+
+    }
+}
+
+class Gato implements Comparable<Gato> {
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "nome=" + nome + ", idade=" + idade + ", cor=" + cor + "}" + "]";
+    }
+
+    @Override
+    public int compareTo(Gato gato) {
+        return this.getNome().compareToIgnoreCase(gato.getNome());
+    }
+
+    static class ComparatorIdade implements Comparator<Gato> {
+        @Override
+        public int compare(Gato g1, Gato g2) {
+            return Integer.compare(g1.getIdade(), g2.getIdade());
+        }
+
+    }
+
+}
---
+++ Associacao/associacaoTeoria/InterfaceExercicio/OperacoesMatematicas/Visibilidade/pacote1/MetodosList/metodos/src/ListaEncadeada.java
@@ -0,0 +1,4 @@
+public class ListaEncadeada<T> {
+    
+    
+}
